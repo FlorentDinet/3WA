eafce878a2e32b4e1af13544072652b5c751a94c
florentDinet <feonentis@hotmail.com>
2016-10-09 16:45:43 +0200

    fin de l'exo1
    je cherche maintenant comment stoper le jeu si le joueur appuie sur
    "annuler"

--- developpement/javascript1/js/exo1.js
+++ developpement/javascript1/js/exo1.js
@@ -5,6 +5,7 @@ var difficulty = 1;
 var min = [1,1,1];
 var max = [15,25,40];
 var chances = [10,8,4];
+var encouragement ="";
 
 // on génère un nombre aléatoire entre min et max inclus
 function getRandomIntInclusive(min, max) {
@@ -14,9 +15,9 @@ function getRandomIntInclusive(min, max) {
 }
 
 // on demande au joueur de choisir un nombre avec gestion d'erreur de saisie
-function askPlayerChoice() {
+function askPlayerChoice(encouragement) {
 
-    playerChoice = parseInt(prompt("deviner ce nombre aléatoire entre " + min[difficulty-1] + " et " + max[difficulty-1]));
+    playerChoice = parseInt(prompt(encouragement + " deviner un nombre aléatoire entre " + min[difficulty-1] + " et " + max[difficulty-1]));
     console.log(playerChoice);
 
     while (!(testPlayerChoice(playerChoice))) {
@@ -27,10 +28,10 @@ function askPlayerChoice() {
 
 }
 
-// on test la valdité du chois du joueur
+// on test la valdité du choix du joueur
 function testPlayerChoice(choice) {
 
-    if (isNaN(parseInt(choice)) || choice > max[difficulty-1] || choice < 0) {
+    if (isNaN(parseInt(choice)) || choice > max[difficulty-1] || choice <= 0) {
         console.log(choice + " is an invalide choice");
 
         return false;
@@ -57,16 +58,22 @@ function game() {
     randomNumber = getRandomIntInclusive(min[difficulty-1], max[difficulty-1]); // on génère un nombre à deviner entre min et max en fonction de la difficulté
     console.log("Le numéro à découvrir est " + randomNumber);
 
-    for (var i = 0; i < chances[difficulty-1]-1; i++) { // on va faire deviner un nombre de fois maximal défini par la difficulté
-        if (i === chances[difficulty-1]-1) {
+    for (var i = 0; i < chances[difficulty-1]+1; i++) { // on va faire deviner un nombre de fois maximal défini par la difficulté
+        if (i === chances[difficulty-1]) {
             alert("Vous avez perdu avec ces combinaisons : " + playerChoices + "\n Il fallait trouver " + randomNumber); // on affiche le message d'echec, les choix du joueur et la solution.
+            playAgain();
             break; // on casse la boucle puisque le jeu est terminé
         }
-        playerChoice = askPlayerChoice(); // on demande son choix au joueur
+        if (i === 0){
+          playerChoice = askPlayerChoice(""); // on demande son choix au joueur
+        } else {
+          playerChoice = askPlayerChoice("Ce n'est pas ça, essayer encore de"); // on demande son choix au joueur
+        }
         playerChoices.push(playerChoice); // on stocke le choix
         console.log("Le joueur a choisi " + playerChoices);
         if (compareNumbers(randomNumber, playerChoice)) { // on demande la comparaison du choix et de la solution
             alert("Vous avez gagné au bout de " + (Number(i) + 1) + " tentatives!"); // on affiche le message de réussite et le nombre de tentatives
+            playAgain();
             break; // on casse la boucle puisque le jeu est terminé
         } else if ((Math.abs(randomNumber - playerChoice)) <= 2) {
             alert("Vous êtes bouillant"); // on affiche le message que le joueur approche de la réponse
@@ -79,11 +86,32 @@ function game() {
 function askDifficulty() {
     difficulty = parseInt(prompt("Choisissez une difficulté 1, 2 ou 3 "));
 
+    while (!(testDifficulty(difficulty))) {
+        difficulty = parseInt(prompt("Choisissez une difficulté 1, 2 ou 3 "));
+    }
+
     return;
+
+}
+
+// on test la valdité de la difficulté
+function testDifficulty(difficulty) {
+
+    if (isNaN(parseInt(difficulty)) || difficulty > min.length || difficulty <= 0) {
+
+        return false;
+    } else {
+
+        return true;
+    }
 }
 
 function playAgain(){
-
+    if (confirm("Voulez-vous rejouer ?")){
+      game(askDifficulty());
+    } else {
+      alert("Merci d'avoir joué !");
+    }
 }
 
 
