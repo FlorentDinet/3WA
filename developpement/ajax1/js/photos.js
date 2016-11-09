6c8fed0893aa4f1e658a38b25c6e25ff3209229b
flodi <florent@flodi.fr>
2016-11-09 19:15:42 +0100

    Ajax 1 fin

--- developpement/ajax1/js/photos.js
+++ developpement/ajax1/js/photos.js
@@ -1,11 +1,36 @@
-$(document).ready (function() {
-  $.getJSON('https://jsonplaceholder.typicode.com/photos')
-  .done(function(datas) {
-    console.log(datas);
-  });
+// albumId: 1
+//
+// id: 1
+//
+// thumbnailUrl: "http://placehold.it/150/30ac17"
+//
+// title: "accusamus beatae ad facilis cum similique qui sunt"
+//
+// url: "http://placehold.it/600/92c952"
+//
 
-  function display(){
-    
-  }
+
+$(document).ready(function() {
+    // on récupère les données
+    $.getJSON('https://jsonplaceholder.typicode.com/photos');
+
+    // on clone notre patron html
+    var cardPatron = $('#cardPatron').clone();
+    cardPatron.attr('id','1');
+
+    //$(".container .row").prepend($("#cardPatron").html());
+
+    function display(albumId, howMuch) {
+
+        var items = [];
+        $.each(data, function(key, val) {
+
+          $('.container .row').prepend(cardPatron);
+        });
+
+
+    }
+
+    // display();
 
 });
